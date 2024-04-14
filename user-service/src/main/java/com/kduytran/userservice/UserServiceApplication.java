69331e5bdafcddd7a2055b37d1391e73acfd64da
kduytran443 <trankhanhduy18@gmail.com>
2024-04-15 00:50:31 +0700

    #user-service: JPA  auditing

--- user-service/src/main/java/com/kduytran/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/kduytran/userservice/UserServiceApplication.java
@@ -1,9 +1,29 @@
 package com.kduytran.userservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "User microservice REST API Documentation",
+				description = "Java Microservices project for online learning platform",
+				version = "v1",
+				contact = @Contact(
+						name = "Tran Khanh Duy",
+						email = "trankhanhduy18@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		)
+)
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
