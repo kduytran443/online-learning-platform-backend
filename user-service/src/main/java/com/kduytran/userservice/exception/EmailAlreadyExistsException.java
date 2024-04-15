1f23829d5aa918ea6b1c97c8c3705cc013fb5e47
kduytran443 <trankhanhduy18@gmail.com>
2024-04-16 01:40:10 +0700

    #user-service: create Exceptions

---
+++ user-service/src/main/java/com/kduytran/userservice/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.kduytran.userservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+public class EmailAlreadyExistsException extends RuntimeException {
+
+    public EmailAlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
