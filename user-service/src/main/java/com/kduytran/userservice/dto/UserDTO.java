04dd99eaee4ef5a71b85094cbaac23ccdf921a1f
kduytran443 <trankhanhduy18@gmail.com>
2024-04-17 07:54:30 +0700

    #user-service: Make dtos

---
+++ user-service/src/main/java/com/kduytran/userservice/dto/UserDTO.java
@@ -0,0 +1,60 @@
+package com.kduytran.userservice.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
+@Schema(
+        name = "UserDTO",
+        description = "Schema to hold User information"
+)
+public class UserDTO {
+    @Schema(
+            description = "Sequence of user", example = "1"
+    )
+    private long seq;
+
+    @Schema(
+            description = "UUID of user", example = "UUID"
+    )
+    private UUID id;
+
+    @Schema(
+            description = "Username of user", example = "duytran"
+    )
+    private String username;
+
+    @Schema(
+            description = "Name of user", example = "Duy Tran"
+    )
+    private String name;
+
+    @Schema(
+            description = "Email of user", example = "test@gmail.com"
+    )
+    private String email;
+
+    @Schema(
+            description = "Type of User", example = "STUDENT"
+    )
+    private String userType;
+
+    @Schema(
+            description = "Status of User", example = "A"
+    )
+    private String userStatus;
+
+    @Schema(
+            description = "Mobile Number of User", example = "0354437687"
+    )
+    private String mobilePhone;
+
+}
