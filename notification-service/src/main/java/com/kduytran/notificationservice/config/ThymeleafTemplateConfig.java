5245590db4da4487d217095695474e50247d943a
kduytran443 <trankhanhduy18@gmail.com>
2024-04-18 23:55:14 +0700

    #notification-service: Config thymeleaf template

---
+++ notification-service/src/main/java/com/kduytran/notificationservice/config/ThymeleafTemplateConfig.java
@@ -0,0 +1,31 @@
+package com.kduytran.notificationservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class ThymeleafTemplateConfig {
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(emailTemplateResolver());
+        return templateEngine;
+    }
+
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver emailTemplateResolver = new ClassLoaderTemplateResolver();
+        emailTemplateResolver.setPrefix("/templates/email/");
+        emailTemplateResolver.setSuffix(".html");
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        emailTemplateResolver.setCacheable(false);
+        return emailTemplateResolver;
+    }
+
+}
