ab5411e607962603fed86d40dd35a11ca120700d
kduytran443 <trankhanhduy18@gmail.com>
2024-04-26 02:39:06 +0700

    #user-service: make controllers for refreshUserVerification and verifyUser API

--- user-service/src/main/java/com/kduytran/userservice/controller/UserController.java
+++ user-service/src/main/java/com/kduytran/userservice/controller/UserController.java
@@ -6,7 +6,7 @@ import com.kduytran.userservice.dto.RegistrationDTO;
 import com.kduytran.userservice.dto.ResponseDTO;
 import com.kduytran.userservice.dto.UserDTO;
 import com.kduytran.userservice.dto.msg.RegistrationMessageDTO;
-import com.kduytran.userservice.producer.RabbitMQProducer;
+import com.kduytran.userservice.event.handler.RabbitMQProducer;
 import com.kduytran.userservice.service.IUserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.websocket.server.PathParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -47,16 +48,12 @@ public class UserController {
 
     @Operation(
             summary = "Create User REST API",
-            description = "REST API to create new User inside the system and Auth Server"
+            description = "REST API to create new User inside the system"
     )
     @ApiResponses({
             @ApiResponse(
                     responseCode = ResponseConstant.STATUS_201,
                     description = "HTTP Status CREATED"
-            ),
-            @ApiResponse(
-                    responseCode = ResponseConstant.STATUS_417,
-                    description = "Expectation Failed"
             ),
             @ApiResponse(
                     responseCode = ResponseConstant.STATUS_500,
@@ -69,6 +66,52 @@ public class UserController {
     @PostMapping("/registration")
     public ResponseEntity<ResponseDTO> registerUser(@RequestBody RegistrationDTO registrationDTO) {
         userService.createUser(registrationDTO);
+        return ResponseEntity.ok(ResponseDTO.of(ResponseConstant.STATUS_201, ResponseConstant.MESSAGE_201));
+    }
+
+    @Operation(
+            summary = "Verify User REST API",
+            description = "REST API to verify User inside the system and Auth Server"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = ResponseConstant.STATUS_201,
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = ResponseConstant.STATUS_500,
+                    description = "HTTP Status INTERNAL SERVER ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
+    @GetMapping("/verification")
+    public ResponseEntity<ResponseDTO> verifyUser(@PathParam("token") String token) {
+        userService.verifyUserRegistration(token);
+        return ResponseEntity.ok(ResponseDTO.of(ResponseConstant.STATUS_201, ResponseConstant.MESSAGE_201));
+    }
+
+    @Operation(
+            summary = "Verify User REST API",
+            description = "REST API to refresh User verification inside the system and Auth Server"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = ResponseConstant.STATUS_201,
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = ResponseConstant.STATUS_500,
+                    description = "HTTP Status INTERNAL SERVER ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
+    @GetMapping("/refresh-verification")
+    public ResponseEntity<ResponseDTO> refreshUserVerification(@PathParam("userId") String userId) {
+        userService.refreshUserVerification(userId);
         return ResponseEntity.ok(ResponseDTO.of(ResponseConstant.STATUS_201, ResponseConstant.MESSAGE_201));
     }
 
--- user-service/src/main/java/com/kduytran/userservice/dto/UserDTO.java
+++ user-service/src/main/java/com/kduytran/userservice/dto/UserDTO.java
@@ -1,8 +1,6 @@
 package com.kduytran.userservice.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
