ff3a943746b77ad90b95c0f0f556a4aeda5cf677
kduytran443 <trankhanhduy18@gmail.com>
2024-04-27 17:31:09 +0700

    #notification-service: bringing duplicated attribute string to constants

---
+++ notification-service/src/main/java/com/kduytran/notificationservice/constant/AttributeConstant.java
@@ -0,0 +1,16 @@
+package com.kduytran.notificationservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class AttributeConstant {
+
+    public static final String USERNAME_ATTRIBUTE = "username";
+    public static final String NAME_ATTRIBUTE = "name";
+    public static final String EMAIL_ATTRIBUTE = "email";
+    public static final String USER_TYPE_ATTRIBUTE = "userType";
+    public static final String TOKEN_ATTRIBUTE = "token";
+    public static final String EXPIRED_DATE_ATTRIBUTE = "expiredDate";
+    public static final String CREATED_AT_ATTRIBUTE = "createdAt";
+
+}
