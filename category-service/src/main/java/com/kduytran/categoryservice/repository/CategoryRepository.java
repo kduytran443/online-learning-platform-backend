684751fea86141657bebfc7c733ef5402c9ca3bd
kduytran443 <trankhanhduy18@gmail.com>
2024-05-04 23:18:33 +0700

    #category-service: Add methods for repository

--- category-service/src/main/java/com/kduytran/categoryservice/repository/CategoryRepository.java
+++ category-service/src/main/java/com/kduytran/categoryservice/repository/CategoryRepository.java
@@ -1,12 +1,18 @@
 package com.kduytran.categoryservice.repository;
 
 import com.kduytran.categoryservice.entity.CategoryEntity;
+import com.kduytran.categoryservice.entity.attrconverter.EntityStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<CategoryEntity, UUID> {
+
+    Optional<CategoryEntity> findByIdAndStatus(UUID id, EntityStatus status);
+    Optional<CategoryEntity> findByCodeAndStatus(String code, EntityStatus status);
+    boolean existsByIdOrCode(UUID id, String code);
 
 }
