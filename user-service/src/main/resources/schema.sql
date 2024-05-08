d6f06eb7be0eb3fbdb3ebefb6a1976bdcc1c340a
kduytran443 <trankhanhduy18@gmail.com>
2024-05-08 23:51:15 +0700

    #user-service: remove "`" from schema.sql

--- user-service/src/main/resources/schema.sql
+++ user-service/src/main/resources/schema.sql
@@ -1,22 +1,22 @@
-CREATE TABLE IF NOT EXISTS `userdata` (
-    `seq` BIGSERIAL,
-    `id` UUID PRIMARY KEY,
-    `username` varchar(200) NOT NULL,
-    `name` varchar(200) NOT NULL,
-    `email` varchar(200) NOT NULL,
-    `user_type` varchar(100) NOT NULL,
-    `user_status` varchar(3) NOT NULL,
-    `mobile_phone` varchar(13) NOT NULL,
-    `created_at` date NOT NULL,
-    `created_by` varchar(20) NOT NULL,
-    `updated_at` date DEFAULT NULL,
-    `updated_by` varchar(20) DEFAULT NULL
+CREATE TABLE IF NOT EXISTS userdata (
+    seq BIGSERIAL,
+    id UUID PRIMARY KEY,
+    username varchar(200) NOT NULL,
+    name varchar(200) NOT NULL,
+    email varchar(200) NOT NULL,
+    user_type varchar(100) NOT NULL,
+    user_status varchar(3) NOT NULL,
+    mobile_phone varchar(13) NOT NULL,
+    created_at date NOT NULL,
+    created_by varchar(20) NOT NULL,
+    updated_at date DEFAULT NULL,
+    updated_by varchar(20) DEFAULT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `user_verification` (
-    `token` varchar(36) PRIMARY KEY,
-    `expired_date` date NOT NULL,
-    `checked` boolean NOT NULL,
-    `user_id` UUID NOT NULL,
-    foreign key (`user_id`) references `userdata`(`id`)
+CREATE TABLE IF NOT EXISTS user_verification (
+    token varchar(36) PRIMARY KEY,
+    expired_date date NOT NULL,
+    checked boolean NOT NULL,
+    user_id UUID NOT NULL,
+    foreign key (user_id) references userdata(id)
 );
