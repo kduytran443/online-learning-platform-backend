d6319d31a61a967c1e4b55cb1b723330bbb21510
kduytran443 <trankhanhduy18@gmail.com>
2024-05-12 23:37:32 +0700

    #class-service: make EntityStatus and OwnerType enum

---
+++ class-service/src/main/java/com/kduytran/classservice/entity/EntityStatus.java
@@ -0,0 +1,27 @@
+package com.kduytran.classservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.stream.Stream;
+
+@Getter
+@AllArgsConstructor
+public enum EntityStatus {
+    LIVE("L"),
+    DELETED("D"),
+    HIDDEN("H");
+
+    private String code;
+
+    public static EntityStatus of(String code) {
+        if (code == null) {
+            throw new RuntimeException();
+        }
+        return Stream.of(EntityStatus.values())
+                .filter(type -> code.equals(type.getCode()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("No enum EntityStatus constant with code " + code));
+    }
+
+}
