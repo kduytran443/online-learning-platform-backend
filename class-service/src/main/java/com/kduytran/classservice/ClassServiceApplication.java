7af281d58ab0c62ba2a27fb598b6092817dd21a0
kduytran443 <trankhanhduy18@gmail.com>
2024-05-12 23:38:57 +0700

    #class-service: Make Spring doc for application

--- class-service/src/main/java/com/kduytran/classservice/ClassServiceApplication.java
+++ class-service/src/main/java/com/kduytran/classservice/ClassServiceApplication.java
@@ -1,9 +1,27 @@
 package com.kduytran.classservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Class microservice REST API Documentation",
+				description = "Java Microservices project for online learning platform",
+				version = "v1",
+				contact = @Contact(
+						name = "Tran Khanh Duy",
+						email = "trankhanhduy18@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		)
+)
 public class ClassServiceApplication {
 
 	public static void main(String[] args) {
