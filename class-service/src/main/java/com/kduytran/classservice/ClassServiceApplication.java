e97394b4fc6bd974cd3dda6821800d4934d1d757
kduytran443 <trankhanhduy18@gmail.com>
2024-05-12 23:39:56 +0700

    #class-service: Make JPA auditing

--- class-service/src/main/java/com/kduytran/classservice/ClassServiceApplication.java
+++ class-service/src/main/java/com/kduytran/classservice/ClassServiceApplication.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
 		info = @Info(
 				title = "Class microservice REST API Documentation",
