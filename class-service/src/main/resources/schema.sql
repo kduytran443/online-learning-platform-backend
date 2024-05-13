3945c1689537f6e6e64734f2371c935be23c4429
kduytran443 <trankhanhduy18@gmail.com>
2024-05-13 23:47:04 +0700

    #class-service: update sql

--- class-service/src/main/resources/schema.sql
+++ class-service/src/main/resources/schema.sql
@@ -4,17 +4,18 @@ CREATE TABLE IF NOT EXISTS class (
     start_at date NOT NULL,
     end_at date,
     status char(1),
-    category_ids UUID[] NOT NULL,
-    group_id UUID,
+    category_id UUID NOT NULL,
+    owner_type char(1),
+    class_owner_id UUID,
     created_at date NOT NULL,
     created_by varchar(20) NOT NULL,
     updated_at date DEFAULT NULL,
-    updated_by varchar(20) DEFAULT NULL
+    updated_by varchar(20) DEFAULT NULL,
 );
 
-CREATE TABLE IF NOT EXISTS class_category (
-    id UUID PRIMARY KEY,
-    content text NOT NULL,
-    class_id UUID NOT NULL,
-    foreign key (class_id) references class(id)
+CREATE TABLE IF NOT EXISTS class_description (
+     id UUID PRIMARY KEY,
+     content text NOT NULL,
+     class_id UUID,
+     foreign key (class_id) references class(id)
 );
