96256dca1759d5316caea6ee2e0479f01be74118
kduytran443 <trankhanhduy18@gmail.com>
2024-05-21 01:56:43 +0700

    #gateway-server: make PathUtils and service constants

--- gateway-server/src/main/java/com/kduytran/gatewayserver/utils/PathUtils.java
+++ gateway-server/src/main/java/com/kduytran/gatewayserver/utils/PathUtils.java
@@ -11,4 +11,20 @@ public class PathUtils {
         return String.format("/%s/%s", CONTEXT_PATH, path);
     }
 
+    public static String getServicePath(String resource) {
+        return getPathWithContextPath(String.format("{versioning:v\\d{1,2}}/%s/**", resource));
+    }
+
+    public static String getRewriteSourcePath(String resource) {
+        return getPathWithContextPath(String.format("(?<versioning>v\\d{1,2})/%s/(?<segment>.*)", resource));
+    }
+
+    public static String getRewriteDestinationPath(String resource) {
+        return String.format("/api/${versioning}/%s/${segment}", resource);
+    }
+
+    public static String getUri(String serviceName) {
+        return String.format("lb://%s", serviceName.toUpperCase());
+    }
+
 }
