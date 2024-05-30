61cc343a173a1cec3c6c031ec958825ca3068b5e
kduytran443 <trankhanhduy18@gmail.com>
2024-05-30 23:55:43 +0700

    #class-service: Make FeeType enum

---
+++ class-service/src/main/java/com/kduytran/classservice/entity/attrconverter/FeeTypeAttributeConverter.java
@@ -0,0 +1,44 @@
+package com.kduytran.classservice.entity.attrconverter;
+
+import com.kduytran.classservice.entity.FeeType;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter(autoApply = true)
+public class FeeTypeAttributeConverter implements AttributeConverter<FeeType, String> {
+
+    /**
+     * Converts the value stored in the entity attribute into the
+     * data representation to be stored in the database.
+     *
+     * @param attribute the entity attribute value to be converted
+     * @return the converted data to be stored in the database
+     * column
+     */
+    @Override
+    public String convertToDatabaseColumn(FeeType attribute) {
+        if (attribute == null) {
+            return null;
+        }
+        return attribute.getCode();
+    }
+
+    /**
+     * Converts the data stored in the database column into the
+     * value to be stored in the entity attribute.
+     * Note that it is the responsibility of the converter writer to
+     * specify the correct <code>dbData</code> type for the corresponding
+     * column for use by the JDBC driver: i.e., persistence providers are
+     * not expected to do such type conversion.
+     *
+     * @param dbData the data from the database column to be
+     *               converted
+     * @return the converted value to be stored in the entity
+     * attribute
+     */
+    @Override
+    public FeeType convertToEntityAttribute(String dbData) {
+        return FeeType.of(dbData);
+    }
+
+}
