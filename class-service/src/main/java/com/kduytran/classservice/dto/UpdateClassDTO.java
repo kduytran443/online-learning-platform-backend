9895d794d884357e5cb387fb4604b831d32ea606
kduytran443 <trankhanhduy18@gmail.com>
2024-06-17 23:56:15 +0700

    #class-service: update validation annotation for UpdateDTO

--- class-service/src/main/java/com/kduytran/classservice/dto/UpdateClassDTO.java
+++ class-service/src/main/java/com/kduytran/classservice/dto/UpdateClassDTO.java
@@ -12,7 +12,6 @@ public class UpdateClassDTO {
 
     @Schema(description = "Name of the class")
     @NotBlank(message = "Name can not be null or empty")
-    @Pattern(regexp = "^[a-z0-9]+[a-z0-9_]{3,15}$", message = "Name is not valid")
     private String name;
 
     @Schema(description = "Banner image of the class", example = "https://example.com/images/banner.jpg")
@@ -34,7 +33,7 @@ public class UpdateClassDTO {
     private String endAt;
 
     @NotBlank(message = "Status can not be null or empty")
-    @Pattern(regexp = "^(Active|Inactive|Cancelled)$", message = "Status is not valid")
+    @Pattern(regexp = "^(L|D|H)$", message = "Status is not valid")
     private String status;
 
     @NotBlank(message = "Category ID can not be null or empty")
@@ -59,10 +58,6 @@ public class UpdateClassDTO {
             example = "This is the name of the class owner."
     )
     @NotEmpty(message = "Class owner name cannot be null or empty")
-    @Pattern(
-            regexp = "^[a-z0-9 ]+$",
-            message = "Class owner name must only contain lowercase letters (a-z), digits (0-9), and spaces"
-    )
     private String ownerName;
 
 }
