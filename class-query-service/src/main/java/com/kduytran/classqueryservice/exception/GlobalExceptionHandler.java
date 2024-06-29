e79df98eb8cb8895b2bde5ad54e05271a7b97493
kduytran443 <trankhanhduy18@gmail.com>
2024-06-30 04:11:36 +0700

    #class-query-service: Make exception handling

---
+++ class-query-service/src/main/java/com/kduytran/classqueryservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.kduytran.classqueryservice.exception;
+
+import com.kduytran.classqueryservice.dto.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handleGlobalException(Exception exception, WebRequest webRequest) {
+        return getErrorResponseEntity(exception, webRequest, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponseDTO> handleResourceNotFoundException(
+            ResourceNotFoundException exception,
+            WebRequest webRequest) {
+        return getErrorResponseEntity(exception, webRequest, HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<ErrorResponseDTO> getErrorResponseEntity(
+            Exception exception, WebRequest webRequest, HttpStatus httpStatus) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(
+                webRequest.getDescription(false),
+                httpStatus,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return ResponseEntity.status(httpStatus).body(errorResponseDTO);
+    }
+
+}
