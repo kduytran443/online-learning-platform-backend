1a515277e6cd5d83c51f54282021112711677d69
kduytran443 <trankhanhduy18@gmail.com>
2024-07-01 23:18:39 +0700

    #class-query-service: update ClassDocument

--- class-query-service/src/main/java/com/kduytran/classqueryservice/document/ClassDocument.java
+++ class-query-service/src/main/java/com/kduytran/classqueryservice/document/ClassDocument.java
@@ -1,19 +1,22 @@
 package com.kduytran.classqueryservice.document;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import com.fasterxml.jackson.databind.ser.std.DateSerializer;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-import org.springframework.data.elasticsearch.annotations.Mapping;
+import org.springframework.data.elasticsearch.annotations.*;
 
-import java.time.LocalDateTime;
+import java.util.Date;
 
 @Document(indexName = "classes")
-@Mapping(mappingPath = "static/classes.json")
 @Getter @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ClassDocument {
     @Id
     private String id;
@@ -24,9 +27,23 @@ public class ClassDocument {
     @Field(type = FieldType.Keyword)
     private String image;
 
-    private LocalDateTime startAt;
+    @Field(type = FieldType.Date, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "GMT+8")
+    @JsonSerialize(using = DateSerializer.class)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+    private Date createdAt;
 
-    private LocalDateTime endAt;
+    @Field(type = FieldType.Date, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "GMT+8")
+    @JsonSerialize(using = DateSerializer.class)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+    private Date startAt;
+
+    @Field(type = FieldType.Date, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX")
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "GMT+8")
+    @JsonSerialize(using = DateSerializer.class)
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
+    private Date endAt;
 
     @Field(type = FieldType.Keyword)
     private String status;
@@ -48,4 +65,11 @@ public class ClassDocument {
 
     @Field(type = FieldType.Text)
     private String ownerName;
+
+    @Field(type = FieldType.Double)
+    private Double averageRating;
+
+    @Field(type = FieldType.Long)
+    private Long numberOfReviews;
+
 }
