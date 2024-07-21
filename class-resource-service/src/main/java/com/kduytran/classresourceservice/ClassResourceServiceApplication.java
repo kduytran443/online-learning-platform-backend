17abd5cfaf923f83049013e5cf75f83e6a4d9e7a
kduytran443 <trankhanhduy18@gmail.com>
2024-07-21 14:36:50 +0700

    #class-resource-service: init project

---
+++ class-resource-service/src/main/java/com/kduytran/classresourceservice/ClassResourceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kduytran.classresourceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassResourceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassResourceServiceApplication.class, args);
+	}
+
+}
---
+++ class-resource-service/src/test/java/com/kduytran/classresourceservice/ClassResourceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kduytran.classresourceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassResourceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
