2bb91e42e1121261e7ae4c99789c454bd5fe15dc
kduytran443 <trankhanhduy18@gmail.com>
2024-07-28 17:10:32 +0700

    #class-resource-service: ActiveProfiles test for application tests

--- class-resource-service/src/test/java/com/kduytran/classresourceservice/ClassResourceServiceApplicationTests.java
+++ class-resource-service/src/test/java/com/kduytran/classresourceservice/ClassResourceServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.kduytran.classresourceservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ClassResourceServiceApplicationTests {
 
 	@Test
