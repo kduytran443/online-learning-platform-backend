e6e9f7d6542934751076b2850f0a7c8934d78613
kduytran443 <trankhanhduy18@gmail.com>
2024-08-03 06:50:17 +0700

    #class-resource-service: impl lesson content

--- class-resource-service/src/main/java/com/kduytran/classresourceservice/dto/LessonContentDTO.java
+++ class-resource-service/src/main/java/com/kduytran/classresourceservice/dto/LessonContentDTO.java
@@ -1,9 +1,12 @@
 package com.kduytran.classresourceservice.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class LessonContentDTO {
+    @NotEmpty
     private String id;
+
     private String content;
 }
