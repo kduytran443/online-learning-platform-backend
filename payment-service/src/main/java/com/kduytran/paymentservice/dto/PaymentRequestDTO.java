7e886a2c321b762dc2b06fd2d58fc76baa6f0909
kduytran443 <trankhanhduy18@gmail.com>
2024-08-24 23:51:32 +0700

    #payment-service: update master card init strategy

--- payment-service/src/main/java/com/kduytran/paymentservice/dto/PaymentRequestDTO.java
+++ payment-service/src/main/java/com/kduytran/paymentservice/dto/PaymentRequestDTO.java
@@ -1,7 +1,6 @@
 package com.kduytran.paymentservice.dto;
 
 import com.kduytran.paymentservice.entity.PaymentMethod;
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
