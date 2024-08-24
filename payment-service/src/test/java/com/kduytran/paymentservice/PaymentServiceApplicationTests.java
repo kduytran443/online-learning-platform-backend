0a5da29c185750cd92577c3c04c4b02f7d823e12
kduytran443 <trankhanhduy18@gmail.com>
2024-08-25 03:52:12 +0700

    #payment-service: config ut environment

--- payment-service/src/test/java/com/kduytran/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/kduytran/paymentservice/PaymentServiceApplicationTests.java
@@ -2,9 +2,17 @@ package com.kduytran.paymentservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("ut")
+@EmbeddedKafka(
+		partitions = 4,
+		brokerProperties = {
+				"listeners=PLAINTEXT://localhost:29092",
+				"port=29092"
+		},
+		topics = {"t_payments", "t_orders"})
 @SpringBootTest
 class PaymentServiceApplicationTests {
 
