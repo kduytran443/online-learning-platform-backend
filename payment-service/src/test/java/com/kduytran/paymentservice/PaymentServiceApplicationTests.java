be33f35e8794f18a2dad00977d0b7f76d2d1f68f
kduytran443 <trankhanhduy18@gmail.com>
2024-08-26 23:53:12 +0700

    #payment-service: update test

--- payment-service/src/test/java/com/kduytran/paymentservice/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/kduytran/paymentservice/PaymentServiceApplicationTests.java
@@ -6,13 +6,7 @@ import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("ut")
-@EmbeddedKafka(
-		partitions = 4,
-		brokerProperties = {
-				"listeners=PLAINTEXT://localhost:29092",
-				"port=29092"
-		},
-		topics = {"t_payments", "t_orders"})
+@EmbeddedKafka
 @SpringBootTest
 class PaymentServiceApplicationTests {
 
