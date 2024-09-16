f630cc5d9503425370ed4978c05dcde86b1d41e5
kduytran443 <trankhanhduy18@gmail.com>
2024-09-17 00:44:21 +0700

    #auth-management-service: init

---
+++ auth-management-service/src/main/java/com/kduytran/authmanagementservice/AuthManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kduytran.authmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthManagementServiceApplication.class, args);
+	}
+
+}
---
+++ auth-management-service/src/test/java/com/kduytran/authmanagementservice/AuthManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kduytran.authmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
