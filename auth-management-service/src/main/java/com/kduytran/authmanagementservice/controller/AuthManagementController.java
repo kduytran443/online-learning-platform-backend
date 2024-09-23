7188964b119e78cd6ef9bff5b56585da175ed01d
kduytran443 <trankhanhduy18@gmail.com>
2024-09-23 21:12:00 +0700

    #auth-management-service: update controller

--- auth-management-service/src/main/java/com/kduytran/authmanagementservice/controller/AuthManagementController.java
+++ auth-management-service/src/main/java/com/kduytran/authmanagementservice/controller/AuthManagementController.java
@@ -1,9 +1,7 @@
 package com.kduytran.authmanagementservice.controller;
 
 import com.kduytran.authmanagementservice.constant.ResponseConstant;
-import com.kduytran.authmanagementservice.dto.ResponseDTO;
-import com.kduytran.authmanagementservice.dto.UserDTO;
-import com.kduytran.authmanagementservice.dto.UserRequestDTO;
+import com.kduytran.authmanagementservice.dto.*;
 import com.kduytran.authmanagementservice.service.IAuthManagementService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -37,17 +35,64 @@ public class AuthManagementController {
     @PutMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseDTO updateUser(@PathVariable String id, @RequestBody @Valid UserRequestDTO dto) {
-        log.info("Updating user with request: userRequestDTO={}", dto);
+        log.info("Updating user with request: id={}, userRequestDTO={}", id, dto);
         authManagementService.updateUser(id, dto);
         return ResponseDTO.of(ResponseConstant.STATUS_200, ResponseConstant.MESSAGE_200);
     }
 
-    @DeleteMapping("/users")
+    @DeleteMapping("/users/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDTO removeUser(@RequestParam String userId) {
-        log.info("Removing user with request: userId={}", userId);
-        authManagementService.removeUser(userId);
+    public ResponseDTO removeUser(@PathVariable String id) {
+        log.info("Removing user with request: id={}", id);
+        authManagementService.removeUser(id);
         return ResponseDTO.of(ResponseConstant.STATUS_200, ResponseConstant.MESSAGE_200);
+    }
+
+    @PostMapping("/resources")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDTO createResource(@RequestBody @Valid ResourceRequestDTO dto) {
+        log.info("Creating resource with request: resourceRequestDTO={}", dto);
+        authManagementService.createResource(dto);
+        return ResponseDTO.of(ResponseConstant.STATUS_201, ResponseConstant.MESSAGE_201);
+    }
+
+    @DeleteMapping("/resources/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO removeResource(@PathVariable String id) {
+        log.info("Removing resource with request: id={}", id);
+        authManagementService.removeResource(id);
+        return ResponseDTO.of(ResponseConstant.STATUS_200, ResponseConstant.MESSAGE_200);
+    }
+
+    @PostMapping("/resource-scopes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDTO addScopesToUser(@RequestBody @Valid AddScopesDTO dto) {
+        log.info("Adding resource scopes to user with request: addScopesDTO={}", dto);
+        authManagementService.addScopesToUser(dto);
+        return ResponseDTO.of(ResponseConstant.STATUS_201, ResponseConstant.MESSAGE_201);
+    }
+
+    @DeleteMapping("/resource-scopes")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO removeScopesFromUser(@RequestBody @Valid RemoveScopesDTO dto) {
+        log.info("Removing resource scopes from user with request: removeScopesDTO={}", dto);
+        authManagementService.removeScopesFromUser(dto);
+        return ResponseDTO.of(ResponseConstant.STATUS_201, ResponseConstant.MESSAGE_201);
+    }
+
+    @DeleteMapping("/resource-scopes/all")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO removeAllScopesFromUser(@RequestBody @Valid RemoveScopesDTO dto) {
+        log.info("Removing all resource scopes from user with request: removeScopesDTO={}", dto);
+        authManagementService.removeAllScopesFromUser(dto);
+        return ResponseDTO.of(ResponseConstant.STATUS_201, ResponseConstant.MESSAGE_201);
+    }
+
+    @GetMapping("/resource-scopes")
+    @ResponseStatus(HttpStatus.OK)
+    public ScopePermissionDTO getScopePermissionOfUser(@RequestParam String userId, @RequestParam String resourceId) {
+        log.info("Getting resource scopes of user with request: id={}, resourceId={}", userId, resourceId);
+        return authManagementService.getScopePermissionOfUser(userId, resourceId);
     }
 
 }
