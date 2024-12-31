83da628d55a20b4aebda676a1a5c640a86757a0d
kduytran443 <trankhanhduy18@gmail.com>
2025-01-01 04:42:01 +0700

    #member-service: update repository to handle findAllClassIdsByUserId

--- member-service/src/main/java/com/kduytran/memberservice/repository/ClassMemberRepository.java
+++ member-service/src/main/java/com/kduytran/memberservice/repository/ClassMemberRepository.java
@@ -2,10 +2,15 @@ package com.kduytran.memberservice.repository;
 
 import com.kduytran.memberservice.entity.ClassMemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface ClassMemberRepository extends JpaRepository<ClassMemberEntity, UUID> {
+
+    @Query("SELECT classId FROM ClassMemberEntity WHERE userId = :userId")
+    Set<UUID> findAllClassIdsByUserId(UUID userId);
 }
