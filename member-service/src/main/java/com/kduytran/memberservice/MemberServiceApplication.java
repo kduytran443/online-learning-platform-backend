9abc7ccae71f53d3406c2821a78d38f1b87937f7
kduytran443 <trankhanhduy18@gmail.com>
2025-01-02 00:19:57 +0700

    #member-service: enable feign client

--- member-service/src/main/java/com/kduytran/memberservice/MemberServiceApplication.java
+++ member-service/src/main/java/com/kduytran/memberservice/MemberServiceApplication.java
@@ -2,12 +2,14 @@ package com.kduytran.memberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.kduytran.memberservice.service.client")
 @SpringBootApplication
 public class MemberServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MemberServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MemberServiceApplication.class, args);
+    }
 
 }
