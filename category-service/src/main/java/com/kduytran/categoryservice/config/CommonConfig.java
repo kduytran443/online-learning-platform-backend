6a05b5e6c139c0cd83ccc6a7fe650b00d638b86a
kduytran443 <trankhanhduy18@gmail.com>
2025-04-27 02:52:35 +0700

    #category-service: integrate CDC kafka debezium

--- category-service/src/main/java/com/kduytran/categoryservice/config/CommonConfig.java
+++ category-service/src/main/java/com/kduytran/categoryservice/config/CommonConfig.java
@@ -3,6 +3,8 @@ package com.kduytran.categoryservice.config;
 import com.kduytran.olpcommon.validation.CommonValidatorService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class CommonConfig {
@@ -10,5 +12,13 @@ public class CommonConfig {
     @Bean
     public CommonValidatorService commonValidatorService() {
         return new CommonValidatorService();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);
+        factory.setConnectionRequestTimeout(5000);
+        return new RestTemplate(factory);
     }
 }
